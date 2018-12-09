d000b66a8df3bad1e7ac5930a8d10364340865ce
jordin513 <jordin513@gmail.com>
2018-12-09 18:58:33 -0500

    Final Commit

--- src/Simon/SimonController.java
+++ src/Simon/SimonController.java
@@ -3,6 +3,12 @@
 package Simon;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -10,16 +16,11 @@ import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.shape.Circle;
-
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
+import javafx.stage.Stage;
 
 public class SimonController {
 
@@ -53,6 +54,8 @@ public class SimonController {
   @FXML
   private Label HighScores;
 
+  String CacheColor;
+
   boolean GameStart;
   static int RandomNum;
   //array of all color buttons
@@ -63,29 +66,117 @@ public class SimonController {
 
   //queue that will hold all color button inputs from user
   Queue<Integer> Player = new LinkedList<Integer>();
+  String highscoreprint;
+  @FXML
+  private Button DeleteButton;
 
-//function that will flash all computer colors in list
-  void FlashColors() {
-    String CacheColor;
+  //function that will flash all computer colors in list
+  void FlashTempColor(int FlashingColor) {
+
+    CacheColor = ColorArray.get(FlashingColor).getStyle();
+    ColorArray.get(FlashingColor).setStyle("-fx-background-color: #7283f4;");
+    Circle.setStyle("-fx-fill: " + CacheColor.substring(22));
+
+  }
+
+  void BackToOriginal(int FlashingColor) {
+    ColorArray.get(FlashingColor).setStyle(CacheColor);
+    Circle.setStyle("-fx-fill: #000000");
+  }
+
+  public void ColorWait() {
+    System.out.println("start wait");
+    try {
+      Thread.sleep(3000);
+    } catch (Exception e) {
+
+    }
+    System.out.println("end wait");
+  }
+
+  //begins game
+  @FXML
+  void StartClick(ActionEvent event) throws Exception {
+    GameStart = true;
+    ColorArray.add(BlueButton);
+    ColorArray.add(GreenButton);
+    ColorArray.add(YellowButton);
+    ColorArray.add(RedButton);
+
+    System.out.println(GameStart);
+    RandomNum = new Random().nextInt(4);
+    Computer.add(RandomNum);
     for (Integer FlashingColor : Computer) {
-      CacheColor = ColorArray.get(FlashingColor).getStyle();
-      ColorArray.get(FlashingColor).setStyle("-fx-background-color: #7283f4;");
+      FlashTempColor(FlashingColor);
+      ColorWait();
+      Stage stage;
+      Parent root;
 
-//     Main.
-//      .requestLayout();
-//Main.primaryStage.refresh();
+      //BackToOriginal(FlashingColor);
+      System.out.println("about to leave");
+    }
+    AccessDatabase("Select");
 
-      System.out.println("start wait");
-      try {
-        Thread.sleep(5000);
-      } catch (Exception e) {
 
-      }
-      System.out.println("end wait");
-      ColorArray.get(FlashingColor).setStyle(CacheColor);
+
+  }
+
+  @FXML
+  void Delete(ActionEvent event) {
+
+  }
+
+  public void AccessDatabase(String query) {
+
+    //database
+    String QueryString;
+    final String DATABASE_URL = "jdbc:derby:lib//books";
+    final String SELECT_QUERY =
+        "SELECT NAME,SCORE From GAMES.HIGHSCORES order by SCORE DESC";
+    final String DELETE_QUERY =
+        "DELETE from GAMES.HIGHSCORES where score = (SELECT MIN(SCORE) from GAMES.HIGHSCORES)";
+
+    if (query.equals("Select")) {
+      QueryString = SELECT_QUERY;
+    } else { //(query=="Delete")
+      QueryString = DELETE_QUERY;
     }
 
+    // use try-with-resources to connect to and query the database
 
+    String deitel = "deitel";
+    try (
+        Connection connection = DriverManager.getConnection(
+            DATABASE_URL, deitel, deitel);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(QueryString)) {
+
+      // get ResultSet's meta data
+      ResultSetMetaData metaData = resultSet.getMetaData();
+      int numberOfColumns = metaData.getColumnCount();
+
+      highscoreprint = "Leaderboard from Database: \n";
+      int count = 0;
+
+      // display query results
+      while (resultSet.next()) {
+        for (int i = 1; i <= numberOfColumns; i++) {
+          highscoreprint += resultSet.getObject(i).toString() + " ";
+          count++;
+
+          if (count == 2) {
+            highscoreprint += "\n";
+            count = 0;
+          }
+        }
+      }
+      HighScores.setText(highscoreprint);
+
+
+    } // AutoCloseable objects' close methods are called now
+    catch (SQLException sqlException) {
+      sqlException.printStackTrace();
+    }
   }
 
   @FXML
@@ -99,30 +190,12 @@ public class SimonController {
   }
 
   @FXML
-  void RedClick(ActionEvent event) {
-
-  }
-
-  //begins game
-  @FXML
-  void StartClick(ActionEvent event) {
-    GameStart = true;
-    ColorArray.add(BlueButton);
-    ColorArray.add(GreenButton);
-    ColorArray.add(YellowButton);
-    ColorArray.add(RedButton);
-
-    System.out.println(GameStart);
-    RandomNum = new Random().nextInt(4);
-    Computer.add(RandomNum);
-    FlashColors();
-
-
-  }
-
-
-  @FXML
   void YellowClick(ActionEvent event) {
+
+  }
+
+  @FXML
+  void RedClick(ActionEvent event) {
 
   }
 
@@ -143,41 +216,7 @@ public class SimonController {
     assert Circle != null : "fx:id=\"Circle\" was not injected: check your FXML file 'Simon.fxml'.";
     GameStart = false;
 
-    //database
-    final String DATABASE_URL = "jdbc:derby:lib//books";
-    final String SELECT_QUERY =
-        "SELECT NAME,SCORE from GAMES.HIGHSCORES";
 
-    // use try-with-resources to connect to and query the database
-    try (
-        Connection connection = DriverManager.getConnection(
-            DATABASE_URL, "deitel", "deitel");
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(SELECT_QUERY)) {
-      // get ResultSet's meta data
-      ResultSetMetaData metaData = resultSet.getMetaData();
-      int numberOfColumns = metaData.getColumnCount();
-
-      String highscoreprint = "Leaderboard from Database: \n";
-      int count = 0;
-
-      // display query results
-      while (resultSet.next()) {
-        for (int i = 1; i <= numberOfColumns; i++) {
-          highscoreprint += resultSet.getObject(i).toString() + " ";
-          count++;
-
-          if (count == 2) {
-            highscoreprint += "\n";
-            count = 0;
-          }
-        }
-      }
-      HighScores.setText(highscoreprint);
-    } // AutoCloseable objects' close methods are called now
-    catch (SQLException sqlException) {
-      sqlException.printStackTrace();
-    }
 
   }
 }
